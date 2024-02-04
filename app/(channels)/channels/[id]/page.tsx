ed1ad12e73505be370155e30287686c32a4e81ba
Ando <iamando@tuta.io>
2024-02-04 16:56:27 +0300

    feat: handle getting channel for message and messages

--- app/(channels)/channels/[id]/page.tsx
+++ app/(channels)/channels/[id]/page.tsx
@@ -1,47 +1,111 @@
 'use client'
 
-import { useEffect } from 'react'
-
+import { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
 import { getCookie } from 'cookies-next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHashtag, faPaperPlane, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { toast } from 'react-toastify'
+
+import LoaderComponent from '@/components/LoaderComponent'
+
+import { getChannel } from '@/actions/channel'
+import { getMessages } from '@/actions/message'
 
 const Page = () => {
+  const { id } = useParams()
+  const userCookie = getCookie('user')
+  const user = userCookie ? JSON.parse(userCookie) : null
+
+  const [messages, setMessages] = useState<Array<{ [key: string]: any }>>([])
+  const [channel, setChannel] = useState<{ slug: string }>({ slug: '' })
+
+  const [isChannelLoading, setIsChannelLoading] = useState<boolean>(false)
+  const [isMessagesLoading, setIsMessagesLoading] = useState<boolean>(false)
+
+  const handleGetChannel = () => {
+    setIsChannelLoading(true)
+
+    getChannel({ channelId: Number(id) })
+      .then(({ error, data }) => {
+        if (error) {
+          toast.error('An error occurred while getting channel')
+          return
+        }
+        setChannel(data[0])
+      })
+      .finally(() => setIsChannelLoading(false))
+  }
+
+  const handleGetMessages = () => {
+    setIsMessagesLoading(true)
+
+    getMessages({ channelId: Number(id) })
+      .then(({ error, data }) => {
+        if (error) {
+          toast.error('An error occurred while getting messages')
+          return
+        }
+        setMessages(data)
+      })
+      .finally(() => setIsMessagesLoading(false))
+  }
+
   useEffect(() => {
-    getCookie('user')
-  }, [])
+    handleGetChannel()
+    handleGetMessages()
+  }, [id])
 
   return (
     <div className='channel__messages'>
       <div className='channel__message_header border-b bg-emerald-500'>
         <div className='text-white py-1 flex items-center ml-4 text-2xl'>
           <FontAwesomeIcon className='mr-2' icon={faHashtag} style={{ fontSize: 24 }} />
-          Channel 1
+          {isChannelLoading ? '...' : <> {channel.slug}</>}
         </div>
       </div>
 
       <div className='channel__message_content'>
-        <div className='channel__message_item py-2'>
-          <div className='mb-2 text-gray-600 text-sm font-extralight'>
-            <span>user@gmail.com</span> - <span>Today 10/19/10</span>
+        {isMessagesLoading ? (
+          <div className='flex justify-center items-center h-full'>
+            <LoaderComponent />
           </div>
-          <div className='channel__message_item_content'>
-            <span className='channel__message_item_content_msg text-white'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus vel nesciunt
-              optio ipsa labore facilis assumenda cumque, facere iste rem id? Quia necessitatibus
-              similique beatae animi reiciendis repellat doloribus tempore.
-            </span>
-            <div className='channel__message_item_content_btn'>
-              <button>
-                <FontAwesomeIcon
-                  className='mr-2 text-gray-600'
-                  icon={faTrash}
-                  style={{ fontSize: 14 }}
-                />
-              </button>
-            </div>
-          </div>
-        </div>
+        ) : (
+          <>
+            {Array.isArray(messages) && messages.length === 0 ? (
+              <div className='flex justify-center items-end h-full'>
+                <div className='bg-emerald-500 w-44 flex justify-center items-center px-4 py-4 rounded-md text-white'>
+                  No message
+                </div>
+              </div>
+            ) : (
+              <>
+                {messages.map((message) => (
+                  <div key={message.id} className='channel__message_item py-2'>
+                    <div className='mb-2 text-gray-600 text-sm font-extralight'>
+                      <span className='font-semibold'>{message.author.username}</span> -
+                      <span className='ml-1'>{new Date(message.inserted_at).toString()}</span>
+                    </div>
+                    <div className='channel__message_item_content'>
+                      <span className='channel__message_item_content_msg text-white'>
+                        {message.message}
+                      </span>
+                      <div className='channel__message_item_content_btn'>
+                        <button>
+                          <FontAwesomeIcon
+                            className='mr-2 text-gray-600'
+                            icon={faTrash}
+                            style={{ fontSize: 14 }}
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </>
+            )}
+          </>
+        )}
       </div>
 
       <div className='channel__message_input border-t'>
