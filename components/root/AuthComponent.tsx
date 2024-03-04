fa6ae1eb58b6a93881b8bb5423b0a4efe467ea5e
Ando <iamando@tuta.io>
2024-03-04 20:36:13 +0300

    feat: add loader on sign and await loader on connected to be set to false

--- components/root/AuthComponent.tsx
+++ components/root/AuthComponent.tsx
@@ -8,6 +8,8 @@ import { setCookie } from 'cookies-next'
 
 import { login, register } from '@/actions/user'
 
+import LoaderComponent from '../LoaderComponent'
+
 const AuthComponent = () => {
   const router = useRouter()
 
@@ -33,7 +35,10 @@ const AuthComponent = () => {
       .finally(() => {
         setEmail('')
         setPassword('')
-        setIsLoading(false)
+
+        setTimeout(() => {
+          setIsLoading(false)
+        }, 500)
       })
   }
 
@@ -73,119 +78,127 @@ const AuthComponent = () => {
         />
       </div>
 
-      <form className='mt-6'>
-        {!isLoggingIn ? (
-          <>
-            <div>
-              <label htmlFor='fullname' className='block text-sm text-gray-800'>
-                Fullname
-              </label>
-              <input
-                onChange={(e) => setFullname(e.target.value)}
-                value={fullname}
-                type='text'
-                name='fullname'
-                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
-              />
-            </div>
-            <div className='mt-4'>
-              <label htmlFor='username' className='block text-sm text-gray-800'>
-                Username
-              </label>
-              <input
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-                type='text'
-                name='username'
-                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
-              />
-            </div>
-          </>
-        ) : null}
-
-        <div className='mt-4'>
-          <label htmlFor='email' className='block text-sm text-gray-800'>
-            Email
-          </label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            type='text'
-            name='email'
-            className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
-          />
+      {isLoading ? (
+        <div className='flex justify-center items-center mt-6'>
+          <LoaderComponent />
         </div>
-
-        <div className='mt-4'>
-          <div className='flex items-center justify-between'>
-            <label htmlFor='password' className='block text-sm text-gray-800'>
-              Password
-            </label>
-          </div>
-
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            type='password'
-            name='password'
-            className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
-          />
-        </div>
-
-        {!isLoggingIn ? (
-          <div className='mt-4'>
-            <div className='flex items-center justify-between'>
-              <label htmlFor='confirmPassword' className='block text-sm text-gray-800'>
-                Confirm Password
-              </label>
-            </div>
-
-            <input
-              onChange={(e) => setPasswordConfirm(e.target.value)}
-              value={passwordConfirm}
-              type='password'
-              name='confirmPassword'
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
-            />
-          </div>
-        ) : null}
-
+      ) : (
         <div className='mt-6'>
-          {isLoggingIn ? (
-            <button
-              onClick={(e) => {
-                e.preventDefault()
-                handleLogin()
-              }}
-              disabled={isLoading}
-              className='w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-lg hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
-            >
-              {isLoading ? 'Loading...' : 'Login'}
-            </button>
-          ) : (
-            <button
-              onClick={(e) => {
-                e.preventDefault()
-                handleRegister()
-              }}
-              disabled={isLoading}
-              className='w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-lg hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
-            >
-              {isLoading ? 'Loading...' : 'Register'}
-            </button>
-          )}
-        </div>
-      </form>
+          <form>
+            {!isLoggingIn ? (
+              <>
+                <div>
+                  <label htmlFor='fullname' className='block text-sm text-gray-800'>
+                    Fullname
+                  </label>
+                  <input
+                    onChange={(e) => setFullname(e.target.value)}
+                    value={fullname}
+                    type='text'
+                    name='fullname'
+                    className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                  />
+                </div>
+                <div className='mt-4'>
+                  <label htmlFor='username' className='block text-sm text-gray-800'>
+                    Username
+                  </label>
+                  <input
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    type='text'
+                    name='username'
+                    className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                  />
+                </div>
+              </>
+            ) : null}
 
-      <p className='mt-8 text-xs font-light text-center text-gray-400'>
-        Dont have an account?
-        <button
-          onClick={() => setIsLoggingIn(!isLoggingIn)}
-          className='font-medium text-emerald-500 hover:underline ml-1'
-        >
-          {!isLoggingIn ? 'Login' : 'Register'}
-        </button>
-      </p>
+            <div className='mt-4'>
+              <label htmlFor='email' className='block text-sm text-gray-800'>
+                Email
+              </label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type='text'
+                name='email'
+                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              />
+            </div>
+
+            <div className='mt-4'>
+              <div className='flex items-center justify-between'>
+                <label htmlFor='password' className='block text-sm text-gray-800'>
+                  Password
+                </label>
+              </div>
+
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type='password'
+                name='password'
+                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              />
+            </div>
+
+            {!isLoggingIn ? (
+              <div className='mt-4'>
+                <div className='flex items-center justify-between'>
+                  <label htmlFor='confirmPassword' className='block text-sm text-gray-800'>
+                    Confirm Password
+                  </label>
+                </div>
+
+                <input
+                  onChange={(e) => setPasswordConfirm(e.target.value)}
+                  value={passwordConfirm}
+                  type='password'
+                  name='confirmPassword'
+                  className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                />
+              </div>
+            ) : null}
+
+            <div className='mt-6'>
+              {isLoggingIn ? (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    handleLogin()
+                  }}
+                  disabled={isLoading}
+                  className='w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-lg hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+                >
+                  {isLoading ? 'Loading...' : 'Login'}
+                </button>
+              ) : (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    handleRegister()
+                  }}
+                  disabled={isLoading}
+                  className='w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-lg hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+                >
+                  {isLoading ? 'Loading...' : 'Register'}
+                </button>
+              )}
+            </div>
+          </form>
+
+          <p className='mt-8 text-xs font-light text-center text-gray-400'>
+            Dont have an account?
+            <button
+              onClick={() => setIsLoggingIn(!isLoggingIn)}
+              className='font-medium text-emerald-500 hover:underline ml-1'
+            >
+              {!isLoggingIn ? 'Login' : 'Register'}
+            </button>
+          </p>
+        </div>
+      )}
     </section>
   )
 }
