638b56b849d8aeabdb50d24169ce30b95daa38c5
Ando <iamando@tuta.io>
2024-03-15 22:37:49 +0300

    feat: impl types for channel , message and direct message

--- typings/common.ts
+++ typings/common.ts
@@ -6,3 +6,26 @@ type TUser = {
   status: string
   username: string
 }
+
+type TChannel = {
+  id: number
+  slug: string
+  created_by: Date
+  inserted_at: Date
+}
+
+type TMessage = {
+  id: number
+  message: string
+  user_id: string
+  channel_id: number
+  inserted_at: number
+}
+
+type TDirectMessage = {
+  id: number
+  message: string
+  sender_id: string
+  receiver_id: string
+  inserted_at: Date
+}
