9580822315d16b5968c27c43ed539a25cde603b1
Ando <iamando@tuta.io>
2024-04-02 20:30:26 +0300

    feat: add to common util get slugified channel name

--- utils/common.ts
+++ utils/common.ts
@@ -5,3 +5,14 @@ export const getInitials = ({ user }: { user: TUser | undefined }): string => {
     ?.join('')
     .toUpperCase() as string
 }
+
+export const getSlugifiedChannelName = ({ channelName }: { channelName: string }): string => {
+  return channelName
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w-]+/g, '') // Remove all non-word chars
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
